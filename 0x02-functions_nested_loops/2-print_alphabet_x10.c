430a8df6e5162d2a437a9c033d8819055b7123db
ugobothankgod <ugobothankgod@gmail.com>
2022-06-08 15:10:51 -0700

    print lowercase alphabets ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,7 +13,7 @@ void print_alphabet_x10(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (alphabet = 'a'; alphabet <= 'z'; j++)
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		{
 		_putchar(alphabet);
 		}
