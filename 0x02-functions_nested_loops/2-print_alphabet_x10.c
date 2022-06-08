b2fdeeb1cdb06e246c2c98db87ac50fe7c7a22f8
ugobothankgod <ugobothankgod@gmail.com>
2022-06-08 15:07:46 -0700

    print lowercase alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints alohabet 10 times
+ *
+ * Return: 0 success
+*/
+
+void print_alphabet_x10(void)
+{
+	int alphabet;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; j++)
+		{
+		_putchar(alphabet);
+		}
+		_putchar('\n');
+	}
+}
