1a75816be78adcf6e0ced3d28bfaf6a0c31f74f8
ugobothankgod <ugobothankgod@gmail.com>
2022-06-14 20:47:42 -0700

    Function to swap two integers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,13 +2,13 @@
 
 /**
  * swap_int - swaps the values of two integers
- * @a: integer to swap
- * @b: integer to swap
+ * @fi: first integer to swap
+ * @si: second integer to swap
  */
 
-void swap_int(int *a, int *b)
+void swap_int(int *fi, int *si)
 {
-	int c = *a;
-	*a = *b;
-	*b = c;
+	int i  = *fi;
+	*fi = *si;
+	*si = i;
 }
