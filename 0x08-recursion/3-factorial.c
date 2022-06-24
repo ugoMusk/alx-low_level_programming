10a9eaad9e0c4495fcbfd8c6ea625733d4e4e89e
ugobothankgod <ugobothankgod@gmail.com>
2022-06-23 17:21:34 -0700

    factorial of a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,13 @@
+ #include "main.h"
+/** - int factorial(int n) - prints the factorial of n
+ * - @n - some random number
+ * - return always 0
+ */
+int factorial(int n)
+{
+	if(n < 0)
+		return(-1);
+	else
+		printf("n!:%d",factorial(n(n-1)));
+	return(0);
+}
