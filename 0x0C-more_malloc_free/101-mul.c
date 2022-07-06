18d46c7a08ed35ffbc2c378050ea96b4c80bd786
ugobothankgod <ugobothankgod@gmail.com>
2022-07-06 16:22:00 -0700

    Multiply two intetgers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,3 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: Always 0.
+ */
+
 int main(int argc, char *argv[])
 {
 unsigned long mul[];
